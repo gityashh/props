f17d545b894d3e4cf72b0568d5ccd0fa30bcad31
gityashh <yashrajput8359@gmail.com>
2024-01-24 01:11:11 +0530

    loader complete

--- script.js
+++ script.js
@@ -24,20 +24,39 @@ function splitter(title) {
 }
 
 
-gsap.from("#title span",{
-    y:500,
-    duration:.6,
-    stagger:.1,
-    ease: "slow(0.7, 0.7, false)",
-    delay:7,
-})
+// gsap.from("#title span",{
+//     y:500,
+//     duration:.6,
+//     stagger:.1,
+//     ease: "slow(0.7, 0.7, false)",
+//     delay:7,
+// })
+// gsap.from("#nav",{
+//     opacity:0,
+//     duration:1.2,
+//     ease: "slow(0.7, 0.7, false)",
+//     delay:8,
+// })
+// gsap.from("#sub",{
+//     opacity:0,
+//     duration:1.2,
+//     ease: "slow(0.7, 0.7, false)",
+//     delay:8,
+// })
+// gsap.from("#vdo",{
+//     scale:3,
+//     duration:1.2,
+//     ease: "slow(0.7, 0.7, false)",
+//     delay:6.5
+// })
+
 
 var image = document.querySelectorAll(".image")
 var tl = gsap.timeline();
 tl.from("imageContainer",{
     width:"0vw",
     duration:1,
-    delay:1.2,
+    delay:2,
     ease:"cubic-bezier(1,-0.01,0,1)"
 })
 tl.to(".image",{
@@ -46,21 +65,43 @@ tl.to(".image",{
     stagger:0.08,
     ease:"cubic-bezier(1,-0.01,0,1)"
 })
+tl.to(".image",{
+    width:"0%",
+    duration:0.5,
+    stagger:-0.08,
+    ease:"cubic-bezier(1,-0.01,0,1)",
+    delay:2
+})
 
-// tl.to(".image",{
-//     width:"0%",
-//     duration:0.5,
-//     stagger:-0.08,
-//     ease:"cubic-bezier(1,-0.01,0,1)",
-//     delay:2
-// })
+gsap.to("#loader",{
+    opacity:0,
+    duration:2,
+    delay:6,
+    ease:"expo.inOut"
+})
 
-// gsap.to("#loader",{
-//     y:"-100%",
-//     duration:2,
-//     delay:20,
-//     ease:"expo.inOut"
-// })
+gsap.to("#percent span",{
+    y:-500,
+    duration:1.2,
+    stagger:.1,
+    ease:"expo.out" ,
+    delay:3,
+})
+gsap.to("#open span",{
+    y:-150,
+    duration:1,
+    stagger:.1,
+    ease: "expo.out",
+    delay:2.9
+})
+gsap.to("#open span",{
+    y:-350,
+    duration:1,
+    stagger:.1,
+    ease: "expo.out",
+    delay:6,
+})
+
 
 
 var grow = 0;
@@ -77,3 +118,8 @@ setTimeout(() => {
     document.querySelector("#grow").style.opacity = 0;
     document.querySelector("#load").style.opacity = 1;
 }, 1500);
+
+setTimeout(() => {
+    document.body.style.overflow = "visible"
+    document.documentElement.style.overflow = "visible"
+}, 5000);
